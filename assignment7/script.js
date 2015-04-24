b67dda9f67ce9d7f7dcb706e6529a613684f56c3
Douglas Neville <doug_neville@hotmail.com>
2015-04-23 22:51:46 -0400

    commit3

--- assignment7/script.js
+++ assignment7/script.js
@@ -1,71 +1,43 @@
-var name = "Doug";
-document.getElementById("Hello Doug").addEventListener('click', function() {
-    console.log("Hello " + +"Doug");
+// < !DOCTYPE html >
+//     < html >
+//     < head >
+//     < title > < /title>
+
+// < script src = "http://code.jquery.com/ui/1.9.2/jquery-ui.js" > < /script> 
+// < script type = "text/javascript"
+// src = "script.js" > </script>
+
+// < /head>
 
 
-}, false);
-
-
-
-document.getElementById("NameArray").addEventListener('click', function() {
-
-    var myArray = ['Bob', 'Steve', 'Jerry', 'Peter'];
-    console.log[1];
-
-}, false);
-
+// < body >
 
 
 
 
-document.getElementById("NumberLoop").addEventListener('click', function() {
-
-    for (i + 0; i < 10; i + 1) {
-        console.log(1);
-    }
-
-}, false);
 
 
 
 
-document.getElementById("LoopNames").addEventListener('click', function() {
 
-    var myArray = ['Bob', 'Steve', 'Jerry', 'Peter'];
-    for (var i = 0; i < myArray.length; i += ) {
-        console.log(i);
-    }
-
-}, false);
-
-
-
-document.getElementById("Ploop").addEventListener('click', function() {
-
-    var myArray = ['Bob', 'Steve', 'Jerry', 'Peter'];
-    var search = function(Peter) {
-        for (var in myArray) {
-            if (myArray === Peter) {
-                console.log(Peter);
-            }
-        }
-    };
-    list(myArray);
-    search("Peter");
-
-
+document.getElementById("button5").addEventListener("click", function() {
+    console.log(myArray);
+    var myArray = ["Bob", "Steve", "Jerry", "Peter"];
+for (i = 0; i < myArray.length; i++); {
+    console.log(myArray[i]);
+}
 }, false);
 
 
 
 
-document.getElementById("car").addEventListener('click', function() {
 
-    var car = {
-        door: 4,
-        honk: function() {
-            console.log("honk");
-        }
-    }
-    car.honk();
-}, false);
+
+
+
+
+
+
+
+
+// < /body> < /html>
---
+++ assignment8/script.js
@@ -0,0 +1,73 @@
+// < !DOCTYPE html >
+//     < html >
+//     < head >
+//     < title > < /title>
+
+// < script src = "http://code.jquery.com/ui/1.9.2/jquery-ui.js" > < /script> 
+// < script type = "text/javascript"
+// src = "script.js" > </script>
+
+// < /head>
+
+
+// < body >
+
+
+
+
+
+
+
+// Array of 4 names//
+
+document.getElementById("button5").addEventListener("click", function() {
+    console.log(myArray);
+    var myArray = ["Bob", "Steve", "Jerry", "Peter"];
+for (i = 0; i < myArray.length; i++); {
+    console.log(myArray[i]);
+}
+}, false);
+
+// For Loop logs 1-10//
+
+document.getElementById("button4").addEventListener("click", function() {
+        var i = 0;
+        for (i = 1; i < 11; i++) {
+            console.log(i);
+        }
+    });
+
+
+    // Create an element you can hover on your page, when hovered it changes to purple//
+
+    $(".blue") .css("color", "purple");
+
+
+
+
+
+    // Select element on my page by its id//
+
+
+
+
+
+
+    // Select an element on your page by it's class//
+
+
+
+
+    // Create a list of items on your page you can iterate through using the Each function//
+
+
+
+
+
+
+
+
+
+
+
+// < /body> < /html>
