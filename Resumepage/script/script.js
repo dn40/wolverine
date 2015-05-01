cf291d403d2aa2a47727e9c45f6d045697e4e693
Douglas Neville <doug_neville@hotmail.com>
2015-04-30 21:32:44 -0400

    commit2

---
+++ Resumepage/script/script.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+
+
+    // countTo//
+    $('.timer').countTo();
+
+
+
+    // Wow.js//
+    new WOW().init();
+
+
+    $(".rotate").textrotator({
+        animation: "spin",
+        speed: 2000
+    });
+
+    $('.collapse').collapse();
+
+    
+});
